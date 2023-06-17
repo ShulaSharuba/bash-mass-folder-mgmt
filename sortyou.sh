d25b45f6bc6745c6c2830ab5063bcec9a8f1f25d
Marc <43393078+ShulaSharuba@users.noreply.github.com>
2023-06-16 19:30:05 -0600

    Add files via upload

---
+++ sortyou.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -p "Enter number of files to match : " fNum
+
+list=()
+  while IFS= read -r line; do
+    list+=( "$line" )
+  done < <( find -type d )
+
+unset list[0] # Remove current dir
+list=( "${list[@]}" ) # Re-index
+
+listLength=${#list[@]}
+  for (( i=0; i<${listLength}; i++ )); do
+    listCount+=( $(find "${list[$i]}" -type f | wc -l) )
+  done
+
+if [[ "${fNum}" != 0 ]]; then
+  for i in "${!list[@]}"; do
+    if [[ "${listCount[i]}" = "${fNum}" ]]; then
+      echo -n "${list[$i]}" | sed 's|.*|"&\/"* |'
+    fi
+  done
+else
+  for i in "${!list[@]}"; do
+    if [[ "${listCount[i]}" = "${fNum}" ]]; then
+      echo -n "${list[$i]}" | sed 's|.*|"&\/" |'
+    fi
+  done
+fi
